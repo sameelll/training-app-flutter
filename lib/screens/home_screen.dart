61ff9e43d4fa35b8500c94b99daf9afb70d43b35
Eren Akel <eren.akel.46@gmail.com>
2021-12-14 10:53:00 +0300

    Assests, json files added, files cleaned, home_screen created

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:training_app/colors.dart' as color;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color.AppColor.homePageBackground,
+    );
+  }
+}
